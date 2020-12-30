0332a8a6ce3905fdc24475fc4f611081e6ce1e0f
Zilola <zilolayakubova.zy@gmail.com>
2020-12-30 14:40:29 +0500

    fix

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'admin_email' => 'admin@gmail.com',
+    'shop_name' => 'Магазин ishop2.loc',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => '587',
+    'smtp_protocol' => 'tls',
+    'smtp_login' => 'receiver@gmail.com',
+    'smtp_password' => 'password',
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'gabordemooij/redbean' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '935cc77ed8bd0f6589b0e296d644ef50c384cf68',
+    ),
+    'vlucas/valitron' => 
+    array (
+      'pretty_version' => 'v1.4.8',
+      'version' => '1.4.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9268adeeb48ba155e35dca861f5990283e14eafb',
+    ),
+  ),
+);
